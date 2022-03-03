203463b555a661607cbb6fc20f01748bf290657e
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-03 10:25:34 +0600

    add responsive css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import "./assets/css/main.css";
+import "./assets/css/main.scss";
 
 
 createApp(App).use(store).use(router).mount('#app');
