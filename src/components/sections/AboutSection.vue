cc0bb577e4d1bf0f2d24dd4db6263aa6f1934ce7
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-03 10:59:47 +0600

    about section design

---
+++ src/components/sections/AboutSection.vue
@@ -0,0 +1,186 @@
+<template>
+<section id="about_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-6 align-self-center">
+                <div class="myImage_slider">
+                    <div class="owl-carousel about_slider">
+                        <div class="single-slider-image">
+                            <div class="img-box">
+                                <img src="" alt="" class="img-fluid" />
+                            </div>
+                            <div class="pos_content">
+                                <span>Shahin</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6">
+                <div class="heading">
+                    <h2 class="mb-0 pb-1">About Me</h2>
+                    <p class="m-0 p-0">My Introduction</p>
+                </div>
+                <div class="tab_buttons border-1 mt-3">
+                    <div class="row">
+                        <div class="col-12">
+                            <nav>
+                                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+                                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+                                </div>
+                            </nav>
+                            <div class="tab-content" id="nav-tabContent">
+                                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi iste pariatur veritatis laudantium rem ab voluptates nulla, odit facere alias, quidem perspiciatis quisquam delectus consequuntur accusantium
+                                    deleniti. Modi, aliquam exercitationem.
+                                </div>
+                                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-12 col-md-4">
+                                                    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                                                        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                                                            Frontend
+                                                        </button>
+                                                        <button
+                                                            class="nav-link"
+                                                            id="v-pills-profile-tab"
+                                                            data-bs-toggle="pill"
+                                                            data-bs-target="#v-pills-profile"
+                                                            type="button"
+                                                            role="tab"
+                                                            aria-controls="v-pills-profile"
+                                                            aria-selected="false"
+                                                        >
+                                                            Backend
+                                                        </button>
+                                                        <button
+                                                            class="nav-link"
+                                                            id="v-pills-messages-tab"
+                                                            data-bs-toggle="pill"
+                                                            data-bs-target="#v-pills-messages"
+                                                            type="button"
+                                                            role="tab"
+                                                            aria-controls="v-pills-messages"
+                                                            aria-selected="false"
+                                                        >
+                                                            Others
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-12 col-md-8" id="abt_skill">
+                                                    <div class="tab-content" id="v-pills-tabContent">
+                                                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                                            <ul>
+                                                                <li><span>HTML5</span></li>
+                                                                <li><span>CSS3</span></li>
+                                                                <li><span>BOOTSTRAP4</span></li>
+                                                                <li><span>WordPress</span></li>
+                                                                <li><span>JAVASCRIPT</span></li>
+                                                                <li><span>JQUERY</span></li>
+                                                                <li><span>Vue</span></li>
+                                                                <li><span>Nuxt</span></li>
+                                                                <li><span>Tailwind CSS</span></li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                                                            <ul>
+                                                                <li><span>PHP</span></li>
+                                                                <li><span>AJAX</span></li>
+                                                                <li><span>AXIOS</span></li>
+                                                                <li><span>MYSQL</span></li>
+                                                                <li><span>LARAVEL</span></li>
+                                                                <li><span>Lumen</span></li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                                                            <ul>
+                                                                <li><span>GIT</span></li>
+                                                                <li><span>GITHUB</span></li>
+                                                                <li><span>GITLAB</span></li>
+                                                                <li><span>Mark Down</span></li>
+                                                                <li><span>Adobe Photoshop</span></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="card" style="border: none;">
+                                                <div class="card-body" style="padding: 5px; padding-top: 8px;">
+                                                    <table class="table table-bordered">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Phone</th>
+                                                                <td><span>dd</span></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th>Email</th>
+                                                                <td>
+                                                                    <span>
+                                                                        <a href="">e</a>
+                                                                        @endif
+                                                                    </span>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th>Address</th>
+                                                                <td><span>f@endif</span></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th>Resume</th>
+                                                                <td><a href="" class="btn btn-link">Resume Link</a></td>
+                                                            </tr>
+                                                        </thead>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+    #about_section ul li{
+        display: inline-block;
+    }
+    #about_section ul li span{
+        background: #F8EFBA;
+        display: inline-block;
+        padding: 5px;
+        font-size: 14px;
+        font-weight: 500;
+        margin-top: 5px;
+        border-radius: 2px;
+        transition: .6s;
+        color: #182C61;
+    }
+    #about_section ul li span:hover{
+        color: #111;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <hero-section/>
-
+    <hr>
+    <about-section/>
+    <hr>
   </div>
 </template>
 
 <script>
-import HeroSection from '@/components/sections/HeroSection.vue'
+import HeroSection from '@/components/sections/HeroSection.vue';
+import AboutSection from '@/components/sections/AboutSection.vue';
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
   components: {
-    HeroSection
+    HeroSection,AboutSection
     
   }
 }
