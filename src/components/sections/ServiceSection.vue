197fdc3175330d3613c06d40005fd1eb9d78ced7
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-03 11:07:51 +0600

    about section design

---
+++ src/components/sections/ServiceSection.vue
@@ -0,0 +1,132 @@
+<template>
+    <section id="service_section">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12">
+                <div class="sec_title wow animate__animated animate__zoomIn">
+                    <h2 class="mb-0 pb-1">My Services</h2>
+                    <p class="m-0 p-0">What i will serve for you!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-12 col-sm-6 col-md-4 mb-4 wow animate__animated animate__lightSpeedInLeft">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(85, 230, 193, 0.3); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                <img src="../../assets/img/about/bug.svg" alt=""
+                                    class="img-fluid" />
+                            </div>
+                            <h4>Web Design</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4 wow animate__animated animate__pulse">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(254, 164, 127, 0.33); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                <img src="../../assets/img/about/web-application.06a0196.svg"
+                                    alt="" class="img-fluid" />
+                            </div>
+                            <h4>Web Development</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-3 wow animate__animated animate__lightSpeedInRight">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(85, 230, 193, 0.3); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                 <img src="../../assets/img/about/web-application.06a0196.svg"
+                                    alt="" class="img-fluid" />
+                            </div>
+                            <h4>Wordpress Customization</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4 wow animate__animated animate__lightSpeedInLeft">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(254, 164, 127, 0.33); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                <img src="../../assets/img/about/web-application.06a0196.svg"
+                                    alt="" class="img-fluid" />
+                            </div>
+                            <h4>Custom Codding</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4 wow animate__animated animate__pulse">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(85, 230, 193, 0.3); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                <img src="../../assets/img/about/web-application.06a0196.svg"
+                                    alt="" class="img-fluid" />
+                            </div>
+                            <h4>Bug Fixing</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4 wow animate__animated animate__lightSpeedInRight">
+                <div class="service_box">
+                    <div class="card" style="background: rgba(254, 164, 127, 0.33); border: none;">
+                        <div class="card-body">
+                            <div class="img_box">
+                                <img src="../../assets/img/about/web-application.06a0196.svg"
+                                    alt="" class="img-fluid" />
+                            </div>
+                            <h4>Graphic Design</h4>
+                            <p>Professional and affordable web design service. I design website with your customer in
+                                mind using latest technology & modern design trends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+#service_section {
+  background: rgba(248, 239, 186, 0.3);
+  text-align: center;
+  .img_box {
+    width: 80px;
+    margin: 0 auto;
+    padding: 8px;
+    border: 2px dotted #1b9cfc;
+  }
+  h4 {
+    margin: 8px 0;
+    font-weight: 600;
+    color: #2c3a47;
+  }
+}
+ 
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,21 @@
     <hr>
     <about-section/>
     <hr>
+    <service-section/>
   </div>
 </template>
 
 <script>
 import HeroSection from '@/components/sections/HeroSection.vue';
 import AboutSection from '@/components/sections/AboutSection.vue';
+import ServiceSection from '@/components/sections/ServiceSection.vue';
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
   components: {
-    HeroSection,AboutSection
+    HeroSection,AboutSection,
+    ServiceSection
     
   }
 }
